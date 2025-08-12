13bd82b3bf217da78e8ac39158227280f576ac35
salvador gh <sal.guerrero.h26@gmail.com>
2025-08-12 16:59:50 -0500

    creacion de carpeta staticfiles para cpanel

--- server_canal_virtual/settings.py
+++ server_canal_virtual/settings.py
@@ -157,10 +157,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173",'https://mi.canalvirtual.pe',"http://192.168.1.207:5173"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173",'https://mi.canalvirtual.pe',"http://192.168.1.227:5173"]
